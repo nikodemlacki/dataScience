347baa06b3f5d6d8b9772cef6d9a94edae3be54d
= <nikodem.lacki@gmail.com>
2016-01-30 14:59:35 +0000

    Minor changes

--- exploratoryAnalysis/plot4.r
+++ exploratoryAnalysis/plot4.r
@@ -11,14 +11,17 @@ pm25_all$year <- factor(pm25_all$year)
 pm25_all$fips <- factor(pm25_all$fips)
 scc$Data.Category <- factor(scc$Data.Category)
 source_types <- unique(scc$Data.Category)
+years <- unique(pm25_all$year)
 
 # Merge SCC data into the subset as it will be needed to generate chart
 pm25_all_extended <- merge(pm25_all, scc, by = "SCC")
-pm25_all_extended <- subset(pm25_all_extended, grepl(x = pm25_all_extended$Short.Name, pattern = "* Coal*"))
-# remove Event and Biogenic data points
-pm25_total_baltimore <-
-  subset(
-    pm25_total_baltimore,
-    pm25_total_baltimore$Data.Category %in% c("Nonpoint", "Nonroad", "Onroad", "Point"))
+pm25_all_extended <- subset(pm25_all_extended, grepl(x = pm25_all_extended$Short.Name, pattern = "*\ Coal*"))
 
+pm25_total_coal <- tapply(
+  pm25_all_extended$Emissions,
+  pm25_all_extended$year,
+  sum)
+pm25_totals_coal <- data.frame(years, pm25_total_coal)
 
+plot(pm25_totals_coal, main="Total PM25 Emissions per year", xlab="Years", ylab="Emmissions")
+lines(pm25_totals_coal)
